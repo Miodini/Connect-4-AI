ba3d67c4c26a5d6392e011f906522fe08d58592c
Miodini <84649065+Miodini@users.noreply.github.com>
2022-05-29 01:20:01 -0300

    Inicio da interface
    Tabuleiro e setas para inserção da ficha adicionados

--- interface.py
+++ interface.py
@@ -1,30 +1,67 @@
-from time import sleep
-import pygame
+import os
+from tokenize import String
+import pygame as pg
 
 class Game:
-    rowsNum = 6
-    colsNum = 7
+    player1Turn = True
+    _margin = 5         # Tamanho da margem entre o tabuleiro e a borda da janela
+    _cellSize = 84      # Tamanho de cada célula. O tabuleiro é dividido em 7 células horizontais e 6 verticais.
+    _sArrowHeight = 22  # Altura do arquivo de imagem da seta pequena
+    _lArrowHeight = 25  # Altura do arquivo de imagem da seta grande
+
     def __init__(self, size):
         self.width, self.height = size
-        pygame.init()
-        self.screen = pygame.display.set_mode(size)
+        pg.init()
+        pg.display.set_caption('Connect 4')
+        self.screen = pg.display.set_mode(size)
         self.screen.fill((255, 255, 255))
-        pygame.display.set_caption('Connect 4')
 
-        self.board = []
-        thickness = 2
-        margin = 20
-        for i in range(self.rowsNum):
-            x = (((self.width - 2*margin)/ self.rowsNum) * i+1) + margin # Posicao de cada linha
-            self.board.append(pygame.Rect(x, 100, thickness, 450))
-        self.board.append(pygame.Rect(self.width - margin, 100, thickness, 450)) # Ultima linha
-        for rect in self.board:
-            self.screen.fill((128,0,0), rect)
+        board = pg.image.load(os.path.join('img', 'board.png')).convert_alpha()
+        self.screen.blit(board, (self._margin, 140))
 
-jogo = Game((600, 600))
-running = True
-while running:
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        self.arrowsPos = []
+        self.smallArrowImg = pg.image.load(os.path.join('img', 'arrow_s.png')).convert_alpha()
+        self.bigArrowImg = pg.image.load(os.path.join('img', 'arrow_l.png')).convert_alpha()
+        for i in range(7):      # 7 colunas
+            self.arrowsPos.append((i*self._cellSize + self._margin, 110))
+            self.drawArrow(i, 'small')        
+
+    def drawArrow(self, i, arrowType):
+        xPos = self.arrowsPos[i][0]
+        yPos = self.arrowsPos[i][1]
+        blankRect = pg.Rect(xPos, yPos, self._cellSize, self._lArrowHeight)
+        pg.draw.rect(self.screen, (255, 255, 255), blankRect)   # Limpa a flecha antiga
+        if arrowType == 'small':
+            self.screen.blit(self.smallArrowImg, (xPos + 10, yPos)) # Cada flecha pequena é 10px menor que cada célula em cada lado
+        elif arrowType == 'big':
+            self.screen.blit(self.bigArrowImg, (xPos, yPos)) # Cada flecha pequena é 10px menor que cada célula em cada lado
+
+    def waitInput(self):
+        pass
+
+    def checkMouseCollision(self):
+        arrowsDownRightPos = list(map(lambda t: (t[0] + self._cellSize, t[1] + self._sArrowHeight), self.arrowsPos)) # Calcula a posição inferior direita de cada flecha
+    
+        mousePos = pg.mouse.get_pos()
+        hovering = False    # Para determinar se o cursor deve mudar ou não
+        for i in range(len(self.arrowsPos)):
+            if mousePos[0] > self.arrowsPos[i][0] and mousePos[1] > self.arrowsPos[i][1] and mousePos[0] < arrowsDownRightPos[i][0] and mousePos[1] < arrowsDownRightPos[i][1]:
+                pg.mouse.set_cursor(pg.SYSTEM_CURSOR_HAND)
+                hovering = True
+                self.drawArrow(i, 'big')
+            else:
+                self.drawArrow(i, 'small')
+        if hovering == False: 
+            pg.mouse.set_cursor(pg.SYSTEM_CURSOR_ARROW)
+
+    def start(self):
+        running = True
+        while running:
+            self.checkMouseCollision()
+            pg.display.update()
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+
+jogo = Game((600, 700))
+jogo.start()
